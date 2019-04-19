e70779be56eb201b37f4285fa4dd5c66aa1d8b99
James Harrington <=>
2019-04-18 22:32:56 -0400

    smaller movies on mobile

--- src/pages/Movies.vue
+++ src/pages/Movies.vue
@@ -10,7 +10,7 @@
       <q-pagination v-model="query.page" :max="pages" :maxPages="9" :directionLinks="true" />
     </div>
     <div class="row q-col-gutter-md q-mt-lg">
-      <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 col-xl-1" v-for="movie in movies" :key="movie.id">
+      <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-1" v-for="movie in movies" :key="movie.id">
         <q-card>
           <q-card-section>
             <q-img :src="movie.medium_cover_image" :ratio="4/6" :placeholder-src="movie.small_cover_image" :style="movie.scary ? 'filter: blur(20px);' : ''" />
